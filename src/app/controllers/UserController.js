3ca156313fd75c9280dc9c9e1a17e8cbf7af0fbc
nicolassaraivaa <nicolas.saraivaa@gmail.com>
2024-09-27 17:34:54 -0300

    fazendo a rota post

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,26 @@
+import {v4} from 'uuid'
+
+import User from '../models/User'
+
+class UserController {
+    async store (req, res) {
+        const {name, email, password_hash,admin} = req.body
+
+        const user = await User.create({
+            id: v4(),
+            name,
+            email,
+            password_hash,
+            admin
+        })
+        console.log(req.body)
+        return res.status(201).json({
+            id:user.id,
+            name,
+            email,
+            admin
+        })
+    }
+}
+
+export default new UserController()
--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,9 @@
 import { Router } from "express"
-import {v4} from 'uuid'
 
-import User from './app/models/User'
+import UserController from "./app/controllers/UserController"
 
 const routes = new Router()
 
-routes.get('/', async (req,res) => {
-    const user = await User.create({
-        id: v4(),
-        name: 'Nicolas',
-        email:'nicolas@gmail.com',
-        password_hash: 'nicolassaraiv27'
-    })
-    return res.status(201).json(user)
-})
+routes.post('/users', UserController.store)
 
 export default routes
